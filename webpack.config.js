57b9c5a52c5392788b6e447f23aec429e5ae702c
gsi-emejias <emejias@generalsoftwareinc.com>
2017-10-08 19:37:37 -0400

    Format webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -11,41 +11,41 @@ let outputFile = 'UserBehaviorLogger';
 let plugins = [];
 
 if (env === 'build') {
-  plugins.push(new UglifyJsPlugin({ minimize: true }));
-  outputFile = outputFile + '.min.js';
+    plugins.push(new UglifyJsPlugin({ minimize: true }));
+    outputFile = outputFile + '.min.js';
 } else {
-  outputFile = outputFile + '.js';
+    outputFile = outputFile + '.js';
 }
 
 const config = {
-  entry: __dirname + '/src/index.js',
-  devtool: 'source-map',
-  output: {
-    path: __dirname + '/lib',
-    filename: outputFile,
-    library: libraryName,
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  module: {
-    rules: [
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: 'babel-loader',
-        exclude: /(node_modules|bower_components)/
-      },
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: 'eslint-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    modules: [path.resolve('./src')],
-    extensions: ['.json', '.js']
-  },
-  plugins: plugins
+    entry: __dirname + '/src/index.js',
+    devtool: 'source-map',
+    output: {
+        path: __dirname + '/lib',
+        filename: outputFile,
+        library: libraryName,
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules|bower_components)/
+            },
+            {
+                test: /(\.jsx|\.js)$/,
+                loader: 'eslint-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        modules: [path.resolve('./src')],
+        extensions: ['.json', '.js']
+    },
+    plugins: plugins
 };
 
 module.exports = config;
